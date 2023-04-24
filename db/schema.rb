c7fb37cae0a01bd88c6117868cf2bfcadc1871ae
ayaka126 <itchy.ayaka@gmail.com>
2023-04-24 20:04:14 +0900

    db:migrate:reset後、テスト

--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,12 @@ ActiveRecord::Schema.define(version: 2023_04_13_040055) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
